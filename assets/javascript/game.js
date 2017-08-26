236f1c5ee5f606126d3498ec723e345980561f09
Stella U <stellau@MeowBook-Pro.local>
2017-08-26 10:52:41 -0700

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 var alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var wins = 0;
 var losses = 0;
-var guessLeft = 10;
-// guessesSoFar is to hold userGuesses each round
+var guessLeft = 9;
+// guessesSoFar is to hold userGuess each round
 var guessSoFar = [];
 // userGuess is what the user picks by pressing each key
 var userGuess = null;
@@ -24,7 +24,7 @@ var updateGuessSoFar = function() {
 
 // Function will be called when reset
 var reset = function() {
-  totalGuesses = 9;
+  userGuess = 9;
   guessesLeft = 9;
   guessSoFar = [];
 
